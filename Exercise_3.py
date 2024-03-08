015295a23d0190d9d69bda842866cb82c9f98e2f
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-08 16:10:19 -0300

    new file:   Exercise_3.py

---
+++ Exercise_3.py
@@ -0,0 +1,10 @@
+# Declaration of variables
+a = float(input())
+b = float(input())
+c = float(input())
+
+# Calculate
+media = ((a*2) + (b*3) + (c*5))/10
+
+# Result
+print('MEDIA = {:.1f}'.format(media))
