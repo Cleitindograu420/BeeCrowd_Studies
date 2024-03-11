8398679f3ff4dfdb6916a629bb81034d04c75a9d
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-11 01:14:18 -0300

    Exercise_1011.py

---
+++ All_Exercises/Exercise_1011.py
@@ -0,0 +1,9 @@
+# Declaration of variable
+radius = float(input())
+pi = 3.14159
+
+# Calculation of the volume of the sphere
+volume = (4/3) * pi * radius**3
+
+#  Printing the result with a fixed number of decimal.
+print('VOLUME = {:.3f}'.format(volume))
