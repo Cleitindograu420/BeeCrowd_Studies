95165c8a217fb57778036f9214ec4df56732792e
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-12 23:37:29 -0300

    Exercise_1037.py Exercise_2604.sql

---
+++ All_Exercises/Exercise_2604.sql
@@ -0,0 +1,4 @@
+  /*  The company's finance department needs a report that shows the code and name of the products whose price is less than 10 or greater than 100.
+  GOT AN ERROR */
+
+select id, name from products where price < 10 or > 100;
