142f9afbd69341d3b0433557f3cf7f73cf8e913a
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-15 22:47:07 -0300

    Exercise_1050.py

--- All_Exercises/ExerciseNAO ACABEI_1050.py
+++
@@ -1,7 +0,0 @@
-num = int(input())
-
-ddd = [61,71,11,21,32,19,27,31]
-local = ['Brasilia','Salvador','Sao Paulo','Rio de Janeiro','Juiz de Fora','Campinas','Vitoria','Belo Horizonte']
-
-print(local.index('Brasilia'))
-print(ddd.index(11))
---
+++ All_Exercises/Exercise_1050.py
@@ -0,0 +1,24 @@
+num = int(input())
+
+# ddd = [61,71,11,21,32,19,27,31]
+# local = ['Brasilia','Salvador','Sao Paulo','Rio de Janeiro','Juiz de Fora','Campinas','Vitoria','Belo Horizonte']
+
+if num == 61:
+    print('Brasilia')
+elif num == 71:
+    print('Salvador')
+elif num == 11:
+    print('Sao Paulo')
+elif num == 21:
+    print('Rio de Janeiro')
+elif num == 32:
+    print('Juiz de Fora')
+elif num == 19:
+    print('Campinas')
+elif num == 27:
+    print('Vitoria')
+elif num == 31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
+    
