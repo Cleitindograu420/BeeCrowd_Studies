0b6e24f13d56eddcc30137be61922c84e6c3baa3
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-15 22:36:39 -0300

    Exercise_1060.py Exercise_1065.py

--- All_Exercises/Exercise_1060.py
+++ All_Exercises/Exercise_1060.py
@@ -1,6 +1,7 @@
 positive = 0
-for a in range(0,6):
+for i in range(0,6):
     a = float(input())
     if a > 0:
         positive += 1
+        print(i) # Do not print on your beecrowd
 print(positive,'valores positivos')
--- All_Exercises/Exercise_1065.py
+++ All_Exercises/Exercise_1065.py
@@ -3,4 +3,6 @@ for i in range (0,5):
     num = int(input())
     if num%2 == 0:
         ct += 1
+        print(i) # Do not print on your beecrowd
 print('{} valores pares'.format(ct))
+
