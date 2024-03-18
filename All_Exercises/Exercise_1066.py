0641642d47cdf61c11232f1cbad5cc7270434004
Henrique Sogayar <henriquesogayar@gmail.com>
2024-03-18 15:08:45 -0300

    Exercise_1066.py

--- All_Exercises/Exercise_1066.py
+++ All_Exercises/Exercise_1066.py
@@ -7,16 +7,18 @@ impar = 0
 for i in range (0,5):
     num = int(input())
     if num > 0:
-        positivo += 1
-        if num % 2 == 0 or num == 0:
-            par += 1
+        positivo = positivo + 1
+        if num %2 == 0:
+            par = par + 1
         else:
-            impar += 1
+            impar = impar + 1
     elif num < 0:
-        negativo += 1
-        if num % 2 == 0:
-            par += 1
+        negativo = negativo + 1
+        if num %2 == 0:
+            par = par + 1
         else:
-            impar += 1
-            print(i) 
+            impar = impar + 1
+            i = i + 1
+    elif num == 0:
+        par = par + 1
 print('{} valor(es) par(es)\n{} valor(es) impar(es)\n{} valor(es) positivo(s)\n{} valor(es) negativo(s)'.format(par,impar,positivo,negativo))
